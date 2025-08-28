0c0e790b9332a5c096955cd142c6aa5680955359
mskrasnov <mskrasnov07@ya.ru>
2025-08-28 05:23:58 +0300

    Reduced the size of the About window

--- src/tui.rs
+++ src/tui.rs
@@ -24,10 +24,25 @@ fn about_window(scr: &mut Cursive) {
         theme::Style::from(theme::Color::Dark(theme::BaseColor::Blue)),
     );
 
-    let summary = Panel::new(LinearLayout::vertical()
-        .child(TextView::new(prog_name))
-        .child(ListView::new().child("Description", TextView::new("A program for calculating the resistance of resistors based on their color marking")).child("Program perository:", TextView::new("https://github.com/mskrasnov/resistor")))
-    ).title("Summary");
+    let summary = Panel::new(
+        LinearLayout::vertical()
+            .child(TextView::new(prog_name))
+            .child(
+                ListView::new()
+                    .child(
+                        "Description:",
+                        TextView::new(
+                            "A program for calculating the resistance of\n\
+                      resistors based on their color marking",
+                        ),
+                    )
+                    .child(
+                        "Program perository:",
+                        TextView::new("https://github.com/mskrasnov/resistor"),
+                    ),
+            ),
+    )
+    .title("Summary");
     let copyright = Panel::new(TextView::new(format!(
         "Copyright (C) 2023-2025 Michail Krasnov <mskrasnov07@ya.ru>"
     )))
